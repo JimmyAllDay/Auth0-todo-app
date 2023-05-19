dad3cb30817b46d515b68be2b6b966b41767513e
James Marshall <jameswhmarshallactions@gmail.com>
2023-05-20 08:43:14 +1000

    added navbar and styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container border border-gray-100 rounded mx-auto my-10 max-w-xl p-2">
+      <Component {...pageProps} />
+    </div>
+  );
 }
